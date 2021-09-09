7cebe72fc2b25225662458a9a07fb114556ea55e
PawatMunkong <pawat.rew@mail.kmutt.ac.th>
2021-09-09 11:29:11 +0000

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,21 +12,20 @@ class Home extends Component {
         
           <SlideHome/>      
           <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/team'} className="nav-link">Team</Link></li>
-            <li><Link to={'/product'} className="nav-link">Product</Link></li>
-          </ul>
-          </nav>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <ul className="navbar-nav mr-auto">
+                <li><Link to={'/team'} className="nav-link">Team</Link></li>
+                <li><Link to={'/product'} className="nav-link">Product</Link></li>
+              </ul>
+            </nav>
           </div>
-
           <div>
           <section class="fdb-block pt-5">
           <div class="container">
           <div class="row justify-content-center">
           <div class="col-12 col-md-8 col-lg-5 text-center pb-md-5 ">
-          <h1>Witpakul Shop</h1>
-          <p class="lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            <h1>Witpakul Shop</h1>
+            <p class="lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
           </div>
             </div>
                </div> 
@@ -41,7 +40,7 @@ class Home extends Component {
         <div class="fdb-box fdb-touch">
           <h2>Step 1</h2>
           <p>Far far away, behind the word mountains, far from the country Vokalia, there live the blind texts.</p>
-     
+   
         </div>
       </div>
       <div class="col-12 col-md-8 m-auto col-lg-4 pt-5 pt-lg-0">
@@ -60,9 +59,9 @@ class Home extends Component {
     </div>
   </div>
 </section>
-          </div>
-  
+          </div>  
         </div>
+        
     );
   }
 }
