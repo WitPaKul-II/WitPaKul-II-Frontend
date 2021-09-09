4880ad705560bd16f502e9841013dbe1f49cfbab
PawatMunkong <pawat.rew@mail.kmutt.ac.th>
2021-09-09 11:32:47 +0000

    Update SlideHome.js

--- src/components/SlideHome.js
+++ src/components/SlideHome.js
@@ -6,13 +6,11 @@ class SlideHome extends Component {
        
               
   <div id="carouselslidehome" class="carousel slide" data-mdb-ride="carousel">
- 
   <div class="carousel-indicators">
     <button type="button" data-mdb-target="#carouselslidehome" data-mdb-slide-to="0" class="active" aria-current="true" aria-label="SlideHome1"></button>
     <button type="button"  data-mdb-target="#carouselslidehome"  data-mdb-slide-to="1" aria-label="SlideHome2" ></button>
     <button type="button" data-mdb-target="#carouselslidehome" data-mdb-slide-to="2" aria-label="SlideHome3" ></button>
   </div>
-
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://dummyimage.com/1600x500/" class="d-block w-100 text-pri"  alt="..." />
@@ -21,7 +19,6 @@ class SlideHome extends Component {
         
       </div>
     </div>
-
     <div class="carousel-item">
       <img src="/assets/image/testOk.png"  class="d-block w-100 text-" alt="..." />
       <div class="carousel-caption d-none d-md-block">
@@ -29,7 +26,6 @@ class SlideHome extends Component {
       
       </div> 
     </div>
-
     <div class="carousel-item">
       <img src="https://dummyimage.com/1600x500/" class="d-block w-100 " alt="..."/>
       <div class="carousel-caption d-none d-md-block">
@@ -37,22 +33,17 @@ class SlideHome extends Component {
       
       </div>
     </div>
-
   </div>
-
   <button class="carousel-control-prev" type="button" data-mdb-target="#carouselslidehome" data-mdb-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden text-da">Previous</span>
   </button>
-
   <button class="carousel-control-next" type="button" data-mdb-target="#carouselslidehome" data-mdb-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
-  </button>
-  
+  </button> 
 </div>
-        
-        
+               
     );
   }
 }
