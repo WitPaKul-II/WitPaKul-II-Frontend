78eb6430b153685ffe6588516c9ed1c1446ba2e8
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-09-10 23:28:44 +0700

    [ADD] update mdb css

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './router/App';
 import reportWebVitals from './reportWebVitals';
-// import '@fortawesome/fontawesome-free/css/all.min.css'; 
-// import 'bootstrap-css-only/css/bootstrap.min.css';
-// import 'mdbreact/dist/css/mdb.css';
-// import "./assets/scss/mdb-free.scss"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
