136d75d83ae0ae4520f7a458650586194b792ce1
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-09-10 17:33:27 +0700

    [FIX] Product mission div ending tag and remove not found css

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,12 +5,13 @@ class Product extends Component {
   render() {
     return (
       <div>
-
         <section class="py-5">
           <div class="container px-4 px-lg-5 my-5 ">
             <div class="row gx-4 gx-lg-5 align-items-center">
               {/*dummyimage*/}
-              <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/300x300/" alt="..." /></div>
+              <div class="col-md-6">
+                <img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/300x300/" alt="..." />
+              </div>
               <div class="col-md-6">
                 {/* BRAND NAME */}
                 <div class="mb-1 fw-bolder">BRAND NAME</div>
@@ -28,13 +29,13 @@ class Product extends Component {
                     <li class="blue"></li>
                   </ul> </div >
                 {/* price && button */}
-                < div class="fs-5 mb-5 d-flex align-items-center fw-bolder text-warning justify-content-between " >
+                <div class="fs-5 mb-5 d-flex align-items-center fw-bolder text-warning justify-content-between " >
                   <span>$40.00</span>
                   <button class=" btn btn-primary btn-lg flex-shrink-0" type="button">
                     <i class="bi-cart-fill me-1"></i>
                     Add to cart
                   </button>
-                </ >
+                </div >
               </div >
             </div >
           </div >
