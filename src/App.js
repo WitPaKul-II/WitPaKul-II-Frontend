d5b9a911354b776314cf1b467ff8b2d1481d287b
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-09-22 21:07:21 +0700

    [FIX] refactor code in App.js, TeamMember.jsx, Home.jsx, and Team.jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route , Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Team from './views/Team';
 
 class App extends Component {
   render() {
     return (
-    <Router>
+      <Router>
         <div>
-          
           <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/team' component={Team} />
+            <Route exact path='/' component={Home} />
+            <Route path='/team' component={Team} />
           </Switch>
         </div>
       </Router>
