93f0aa89506e7f75bc143b15d2a07ea441b7adb4
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-09-22 22:37:02 +0700

    [ADD] renew Navbar to new environment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBFormInline
+} from "mdbreact";
+
+class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    return (
+      <div>
+        <MDBNavbar color="default-color" dark expand="md">
+          <MDBNavbarBrand>
+            <MDBNavLink to="/">
+              <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" style={{ height: "15px" }} alt="" loading="lazy" />
+            </MDBNavLink>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem>
+                <MDBNavLink to="/team">TEAM</MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBFormInline waves>
+                  <div className="md-form my-0">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                  </div>
+                </MDBFormInline>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle nav caret>
+                    <MDBIcon icon="user" />
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu className="dropdown-default">
+                    <MDBDropdownItem href="#!">My profile</MDBDropdownItem>
+                    <MDBDropdownItem href="#!">Settings</MDBDropdownItem>
+                    <MDBDropdownItem href="#!">Logout</MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
--- src/views/Team.jsx
+++ src/views/Team.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
 import TeamMember from '../components/TeamMember';
 
 class Team extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <TeamMember />
       </div>
     );
