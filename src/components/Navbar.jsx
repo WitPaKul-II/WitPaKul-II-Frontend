d15b8a0a839912e81b3cd8ee5aa29510847733e2
Luck <pawat.rew@mail.kmutt.ac.th>
2021-09-23 23:19:05 +0700

    Test build2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,63 @@
-// import React, { Component } from 'react';
-// import {
-//   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-//   MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBFormInline
-// } from "mdbreact";
+import React, { Component } from 'react';
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBFormInline
+} from "mdbreact";
 
-// class Navbar extends Component {
-//   state = {
-//     isOpen: false
-//   };
+class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
 
-//   toggleCollapse = () => {
-//     this.setState({ isOpen: !this.state.isOpen });
-//   }
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <MDBNavbar color="default-color" dark expand="md">
-//           <MDBNavbarBrand>
-//             <MDBNavLink to="/">
-//               <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" style={{ height: "15px" }} alt="" loading="lazy" />
-//             </MDBNavLink>
-//           </MDBNavbarBrand>
-//           <MDBNavbarToggler onClick={this.toggleCollapse} />
-//           <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-//             <MDBNavbarNav left>
-//               <MDBNavItem>
-//                 <MDBNavLink to="/team">TEAM</MDBNavLink>
-//               </MDBNavItem>
-//               <MDBNavItem>
-//                 <MDBNavLink to="/shop">SHOP</MDBNavLink>
-//               </MDBNavItem>
-//             </MDBNavbarNav>
-//             <MDBNavbarNav right>
-//               <MDBNavItem>
-//                 <MDBFormInline waves>
-//                   <div className="md-form my-0">
-//                     <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-//                   </div>
-//                 </MDBFormInline>
-//               </MDBNavItem>
-//               <MDBNavItem>
-//                 <MDBDropdown>
-//                   <MDBDropdownToggle nav caret>
-//                     <MDBIcon icon="user" />
-//                   </MDBDropdownToggle>
-//                   <MDBDropdownMenu className="dropdown-default">
-//                     <MDBDropdownItem href="#!">My profile</MDBDropdownItem>
-//                     <MDBDropdownItem href="#!">Settings</MDBDropdownItem>
-//                     <MDBDropdownItem href="#!">Logout</MDBDropdownItem>
-//                   </MDBDropdownMenu>
-//                 </MDBDropdown>
-//               </MDBNavItem>
-//             </MDBNavbarNav>
-//           </MDBCollapse>
-//         </MDBNavbar>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <MDBNavbar color="default-color" dark expand="md">
+          <MDBNavbarBrand>
+            <MDBNavLink to="/">
+              <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" style={{ height: "15px" }} alt="" loading="lazy" />
+            </MDBNavLink>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem>
+                <MDBNavLink to="/team">TEAM</MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="/shop">SHOP</MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBFormInline waves>
+                  <div className="md-form my-0">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                  </div>
+                </MDBFormInline>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle nav caret>
+                    <MDBIcon icon="user" />
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu className="dropdown-default">
+                    <MDBDropdownItem href="#!">My profile</MDBDropdownItem>
+                    <MDBDropdownItem href="#!">Settings</MDBDropdownItem>
+                    <MDBDropdownItem href="#!">Logout</MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBNavbar>
+      </div>
+    );
+  }
+}
 
-// export default Navbar;
+export default Navbar;
--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,234 +1,234 @@
-// import React, { Component } from 'react';
-// import { MDBContainer, MDBNavLink } from 'mdbreact';
+import React, { Component } from 'react';
+import { MDBContainer, MDBNavLink } from 'mdbreact';
 
-// class ProductItems extends Component {
-//   render() {
-//     return (
-//       // <MDBContainer breakpoint="md" >
-//         <div class="p-5">
-//           <section class="text-center mb-4">
-//             <div class="row wow fadeIn">
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   {/* <MDBNavLink to="/product/1"> */}
-//                     <div class="view overlay">
-//                       {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" class="card-img-top" alt="" /> */}
-//                       <a>
-//                         <div class="mask rgba-white-slight"></div>
-//                       </a>
-//                     </div>
+class ProductItems extends Component {
+  render() {
+    return (
+      <MDBContainer breakpoint="md" >
+        <div class="p-5">
+          <section class="text-center mb-4">
+            <div class="row wow fadeIn">
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <MDBNavLink to="/product/1">
+                    <div class="view overlay">
+                      <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" class="card-img-top" alt="" />
+                      <a>
+                        <div class="mask rgba-white-slight"></div>
+                      </a>
+                    </div>
 
-//                     <div class="card-body text-center">
-//                       <a href="" class="grey-text">
-//                         <h5>Shirt</h5>
-//                       </a>
-//                       <h5>
-//                         <strong>
-//                           <a href="" class="dark-grey-text">Denim shirt
-//                             <span class="badge badge-pill danger-color">NEW</span>
-//                           </a>
-//                         </strong>
-//                       </h5>
+                    <div class="card-body text-center">
+                      <a href="" class="grey-text">
+                        <h5>Shirt</h5>
+                      </a>
+                      <h5>
+                        <strong>
+                          <a href="" class="dark-grey-text">Denim shirt
+                            <span class="badge badge-pill danger-color">NEW</span>
+                          </a>
+                        </strong>
+                      </h5>
 
-//                       <h4 class="font-weight-bold blue-text">
-//                         <strong>120$</strong>
-//                       </h4>
-//                     </div>
-//                   {/* </MDBNavLink> */}
-//                 </div>
-//               </div>
+                      <h4 class="font-weight-bold blue-text">
+                        <strong>120$</strong>
+                      </h4>
+                    </div>
+                  </MDBNavLink>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Sport wear</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Sweatshirt</a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>139$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Sport wear</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Sweatshirt</a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>139$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Sport wear</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Grey blouse
-//                           <span class="badge badge-pill primary-color">bestseller</span>
-//                         </a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>99$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Sport wear</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Grey blouse
+                          <span class="badge badge-pill primary-color">bestseller</span>
+                        </a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>99$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Shirt</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Denim shirt
-//                           <span class="badge badge-pill danger-color">NEW</span>
-//                         </a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>120$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Shirt</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Denim shirt
+                          <span class="badge badge-pill danger-color">NEW</span>
+                        </a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>120$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Sport wear</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Sweatshirt</a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>139$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Sport wear</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Sweatshirt</a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>139$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Sport wear</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Grey blouse
-//                           <span class="badge badge-pill primary-color">bestseller</span>
-//                         </a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>99$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Sport wear</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Grey blouse
+                          <span class="badge badge-pill primary-color">bestseller</span>
+                        </a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>99$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
 
-//               <div class="col-lg-4 col-md-6 mb-4">
-//                 <div class="card">
-//                   <div class="view overlay">
-//                     {/* <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" class="card-img-top" alt="" /> */}
-//                     <a>
-//                       <div class="mask rgba-white-slight"></div>
-//                     </a>
-//                   </div>
-//                   <div class="card-body text-center">
-//                     <a href="" class="grey-text">
-//                       <h5>Outwear</h5>
-//                     </a>
-//                     <h5>
-//                       <strong>
-//                         <a href="" class="dark-grey-text">Black jacket</a>
-//                       </strong>
-//                     </h5>
-//                     <h4 class="font-weight-bold blue-text">
-//                       <strong>219$</strong>
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </section>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                  <div class="view overlay">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg" class="card-img-top" alt="" />
+                    <a>
+                      <div class="mask rgba-white-slight"></div>
+                    </a>
+                  </div>
+                  <div class="card-body text-center">
+                    <a href="" class="grey-text">
+                      <h5>Outwear</h5>
+                    </a>
+                    <h5>
+                      <strong>
+                        <a href="" class="dark-grey-text">Black jacket</a>
+                      </strong>
+                    </h5>
+                    <h4 class="font-weight-bold blue-text">
+                      <strong>219$</strong>
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 
-//           <nav class="d-flex justify-content-center wow fadeIn">
-//             <ul class="pagination pg-blue">
-//               <li class="page-item disabled">
-//                 <a class="page-link" href="#" aria-label="Previous">
-//                   <span aria-hidden="true">&laquo;</span>
-//                   <span class="sr-only">Previous</span>
-//                 </a>
-//               </li>
+          <nav class="d-flex justify-content-center wow fadeIn">
+            <ul class="pagination pg-blue">
+              <li class="page-item disabled">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
 
-//               <li class="page-item active">
-//                 <a class="page-link" href="#">1
-//                   <span class="sr-only">(current)</span>
-//                 </a>
-//               </li>
-//               <li class="page-item">
-//                 <a class="page-link" href="#">2</a>
-//               </li>
-//               <li class="page-item">
-//                 <a class="page-link" href="#">3</a>
-//               </li>
-//               <li class="page-item">
-//                 <a class="page-link" href="#">4</a>
-//               </li>
-//               <li class="page-item">
-//                 <a class="page-link" href="#">5</a>
-//               </li>
+              <li class="page-item active">
+                <a class="page-link" href="#">1
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">2</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">3</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">4</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">5</a>
+              </li>
 
-//               <li class="page-item">
-//                 <a class="page-link" href="#" aria-label="Next">
-//                   <span aria-hidden="true">&raquo;</span>
-//                   <span class="sr-only">Next</span>
-//                 </a>
-//               </li>
-//             </ul>
-//           </nav>
-//         </div>
-//       // {/* </MDBContainer> */}
-//      );
-//   }
-// }
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      // </MDBContainer>
+     );
+  }
+}
 
-// export default ProductItems;
+export default ProductItems;
