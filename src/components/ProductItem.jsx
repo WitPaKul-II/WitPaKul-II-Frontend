acf3e45b9c4977853c28286dd278488787a0e033
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-09-27 16:04:36 +0700

    [FIX] fix product item image size

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import { MDBNavLink } from 'mdbreact';
 
 class ProductItem extends Component {
-  getRandomInt(start, end, step) {
-    return Math.floor(Math.random() * (end-start) / step) * step + start;
-  }
   render() {
     return (
       <div class="col-lg-4 col-md-6 mb-4">
         <div class="card">
           <MDBNavLink to={"/product/" + this.props.data.product_code}>
             <div class="view overlay">
-              <img src={"https://dummyimage.com/" + this.getRandomInt(250, 350, 10) + "x" + this.getRandomInt(350, 400, 10)} class="card-img-top" alt="" />
+              <img src={"https://dummyimage.com/300x400"} class="card-img-top" alt="" />
               <div class="mask rgba-white-slight"></div>
             </div>
             <div class="card-body text-center">
