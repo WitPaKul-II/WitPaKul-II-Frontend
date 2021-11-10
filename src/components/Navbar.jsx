624d49c8dca4faad2d20e4065f8ba21a3817e51f
Luck <pawat.rew@mail.kmutt.ac.th>
2021-11-10 20:44:53 +0700

    [FIX] Fix unused code Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,83 +1,3 @@
-// import React, { Component } from 'react';
-// import {
-//   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-//   MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBFormInline
-// } from "mdbreact";
-
-// class Navbar extends Component {
-//   state = {
-//     isOpen: false,
-//     searchText: ''
-//   };
-
-//   toggleCollapse = () => {
-//     this.setState({ isOpen: !this.state.isOpen });
-//   }
-
-//   handleSearchChange = (event) =>  {
-//     this.setState({ searchText: event.target.value });
-//     if (this.props.onChange) {
-//       this.props.onChange(event);
-//     }
-//   }
-
-//   render() {
-//     var search = <div/>
-//     if (this.props.search) {
-//       search = (
-//         <MDBNavItem >
-//           <MDBFormInline waves>
-//             <div className="md-form my-0">
-//               <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" value={this.state.searchText} onChange={this.handleSearchChange} />
-//             </div>
-//           </MDBFormInline>
-//         </MDBNavItem>
-//       );
-//     }
-//     return (
-//       <div>
-//         <MDBNavbar color="default-color" dark expand="md">
-//           <MDBNavbarBrand>
-//             <MDBNavLink to="/">
-//               <img src="/assets/image/Among2.png" style={{ height: "35px" }} alt="" loading="lazy" />
-//             </MDBNavLink>
-//           </MDBNavbarBrand>
-//           <MDBNavbarToggler onClick={this.toggleCollapse} />
-//           <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-//             <MDBNavbarNav left>
-//               <MDBNavItem>
-//                 <MDBNavLink to="/team">TEAM</MDBNavLink>
-//               </MDBNavItem>
-//               <MDBNavItem>
-//                 <MDBNavLink to="/shop">SHOP</MDBNavLink>
-//               </MDBNavItem>
-//             </MDBNavbarNav>
-//             <MDBNavbarNav right>
-//               {search}
-//               <MDBNavItem>
-//                 <MDBDropdown>
-//                   <MDBDropdownToggle nav caret>
-//                     <MDBIcon icon="user" />
-//                   </MDBDropdownToggle>
-//                   <MDBDropdownMenu right className="dropdown-default">
-//                     <MDBNavLink to="signin"><MDBDropdownItem>Login</MDBDropdownItem></MDBNavLink>
-//                     <MDBNavLink to="login"><MDBDropdownItem>Login2</MDBDropdownItem></MDBNavLink>
-//                     <MDBNavLink to="register"><MDBDropdownItem>Register</MDBDropdownItem></MDBNavLink>
-//                     <MDBDropdownItem href="#!">Settings</MDBDropdownItem>
-//                   </MDBDropdownMenu>
-//                 </MDBDropdown>
-//               </MDBNavItem>
-//             </MDBNavbarNav>
-//           </MDBCollapse>
-//         </MDBNavbar>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Navbar;
-
-
 import React, { useEffect, useCallback, useState } from 'react';
 import {
   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
@@ -89,13 +9,8 @@ import { logout } from "../store/actions/auth";
 import { clearMessage } from "../store/actions/message";
 import { history } from "../store/helpers/history";
 
-
-
 const Navbar = (props) => {
-  // const state = {
-  //   isOpen: false,
-  //   searchText: ''
-  // };
+
   const [searchText, setSearchText] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
@@ -104,7 +19,6 @@ const Navbar = (props) => {
   }
 
   const handleSearchChange = (event) => {
-    // this.setState({ searchText: event.target.value });
     setSearchText(event.target.value)
     if (props.onChange) {
       props.onChange(event);
@@ -146,7 +60,6 @@ const Navbar = (props) => {
     };
   }, [currentUser, logOut]);
  
-  // render() {
   var search = <div />
   if (props.search) {
     search = (
