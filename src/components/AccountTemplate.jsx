23fa0b3822fca6d80d0ede2e7da573f44e2081b6
Sathipp <sakulthip.r@mail.kmutt.ac.th>
2021-11-11 21:34:25 +0700

    [ADD] upload profile image

--- src/components/AccountTemplate.jsx
+++ src/components/AccountTemplate.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import { MDBBtn, MDBIcon } from "mdbreact";
+import { MDBBtn, MDBIcon, MDBInput } from "mdbreact";
 import DatePicker from 'react-datepicker';
 
 class AccountTemplate extends Component {
@@ -8,10 +8,12 @@ class AccountTemplate extends Component {
     super(props);
     this.state = {
       user: {},
+      imageFileURL: "/assets/image/NoImage.png",
+      imageFile: null,
       editUser: {},
       editFlag: false
     };
-    this.handleUploadImage = this.handleUploadImage.bind(this)
+    this.handleImageFileChange = this.handleImageFileChange.bind(this)
     this.handleEditProfile = this.handleEditProfile.bind(this)
     this.handleFirstnameChange = this.handleFirstnameChange.bind(this)
     this.handleLastnameChange = this.handleLastnameChange.bind(this)
@@ -22,8 +24,11 @@ class AccountTemplate extends Component {
     this.handleSave = this.handleSave.bind(this)
     this.handleCancel = this.handleCancel.bind(this)
   }
-  handleUploadImage() {
-
+  handleImageFileChange(event) {
+    this.setState({
+      imageFileURL: URL.createObjectURL(event.target.files[0]),
+      imageFile: event.target.files[0]
+    })
   }
   handleEditProfile() {
     var { user } = this.state;
@@ -60,14 +65,54 @@ class AccountTemplate extends Component {
   handleBirthDateChange(value) {
     var { editUser } = this.state
     if (value) {
-      editUser.birth_date = value
+      editUser._birth_date = value
+      var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+      editUser.birth_date = (new Date(editUser._birth_date - tzoffset)).toISOString().slice(0, -1);
       this.setState({ editUser: editUser })
     }
   }
   handleSave() {
-    var { editUser } = this.state
+    var { editUser, imageFile } = this.state
+    var token = localStorage.getItem("token");
+    if (imageFile) {
+      var formData = new FormData();
+      editUser.user_image_url = editUser.username + "-" + (new Date()).getTime()
+      formData.append("image", imageFile);
+      axios.post(process.env.REACT_APP_BACKEND + "users/images", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data;charset=utf-8',
+          'Authorization': `Bearer ${token}`,
+          'filename': editUser.user_image_url
+        }
+      }).catch(error => {
+        console.log(error)
+      });
+    }
+    console.log(editUser)
+    axios.put(
+      process.env.REACT_APP_BACKEND + "users/edit", 
+      JSON.stringify(editUser), 
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        }
+      }
+    ).then(response => {
+      axios.get(process.env.REACT_APP_BACKEND + "me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      }).then((response_user) => {
+        localStorage.setItem("user", JSON.stringify(response_user.data));
+      })
+      
+    }).catch(error => {
+      console.log(error)
+    });
     this.setState({ 
-      user: editUser, 
+      user: editUser,
+      editUser: editUser, 
       editFlag: false 
     })
   }
@@ -75,30 +120,28 @@ class AccountTemplate extends Component {
     this.setState({ editFlag: false })
   }
   componentWillMount() {
-    // TODO: Load session token and nessesary infomation here
-    // const session_token = "";
-    const user_id = 2;
-
-    const url = process.env.REACT_APP_BACKEND + 'users/findAll/';
-    axios.get(url).then(res => {
-      var user = res.data.filter(obj => { return obj.user_id === user_id })[0]
-      user.birth_date = new Date(user.birth_date)
-      this.setState({
-        user: user
-      });
+    var user = JSON.parse(localStorage.getItem("user"));
+    user._birth_date = new Date(user.birth_date)
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    user.birth_date = (new Date(user._birth_date - tzoffset)).toISOString().slice(0, -1);
+    this.setState({
+      user: user,
+      imageFileURL: process.env.REACT_APP_BACKEND + 'users/images/' + user.user_image_url
     });
   }
   render() {
     var { user, editFlag, editUser } = this.state;
-    console.log(user)
-    console.log(editUser)
-
     var firstname = (<p className="text-muted mb-0">{user.firstname}</p>)
     var lastname = (<p className="text-muted mb-0">{user.lastname}</p>)
     var email = (<p className="text-muted mb-0">{user.email}</p>)
     var tel = (<p className="text-muted mb-0">{user.tel}</p>)
     var address = (<p className="text-muted mb-0">{user.address}</p>)
-    var birth_date = (<p className="text-muted mb-0">{(user.birth_date) ? user.birth_date.toDateString() : ""}</p>);
+    var birth_date = (<p className="text-muted mb-0">{(user._birth_date) ? user._birth_date.toDateString() : ""}</p>);
+    var images = (
+      <div>
+        <img className="rounded-circle img-fluid" src={this.state.imageFileURL} alt="..." />
+      </div>
+    )
     var edit_button = (
       <div className="d-flex justify-content-center mb-2">
         <MDBBtn color="default" onClick={this.handleEditProfile} >
@@ -112,8 +155,12 @@ class AccountTemplate extends Component {
       email = (<input className="form-control mr-sm-2" type="text" placeholder="Email" aria-label="Email" value={this.state.editUser.email} onChange={this.handleEmailChange} />)
       tel = (<input className="form-control mr-sm-2" type="text" placeholder="Telephone" aria-label="Telephone" value={this.state.editUser.tel} onChange={this.handleTelChange} />)
       address = (<input className="form-control mr-sm-2" type="text" placeholder="Address" aria-label="Address" value={this.state.editUser.address} onChange={this.handleAddressChange} />)
-      birth_date = (<DatePicker selected={this.state.editUser.birth_date} onChange={this.handleBirthDateChange} />)
+      birth_date = (<DatePicker selected={this.state.editUser._birth_date} onChange={this.handleBirthDateChange} />)
       edit_button = (
+        <div>
+        <div className="d-flex justify-content-center mb-2">
+          <MDBInput type="file" accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*" onChange={this.handleImageFileChange} />
+        </div>
         <div className="d-flex justify-content-center mb-2">
           <MDBBtn color="default" onClick={this.handleSave} >
             <MDBIcon icon="save" /> Save
@@ -121,6 +168,7 @@ class AccountTemplate extends Component {
           <MDBBtn color="danger" onClick={this.handleCancel} >
             <MDBIcon icon="times-circle" /> Cancel
           </MDBBtn>
+        </div>
         </div>
       )
     }
@@ -133,14 +181,10 @@ class AccountTemplate extends Component {
               <div className="col-lg-4">
                 <div className="card mb-4">
                   <div className="card-body text-center">
-                    <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-chat/ava3.png" alt="avatar" className="rounded-circle img-fluid" style={{ width: "150px;" }} />
+                    {images}
                     <h5 className="my-3">{user.username}</h5>
                     <p className="text-muted mb-1">{(user.user_type) ? user.user_type.type_name : ""} {(user.user_type) ? user.user_type.type_id : ""}</p>
-                    <div className="d-flex justify-content-center mb-2">
-                      <MDBBtn color="default" onClick={this.handleUploadImage} >
-                        <MDBIcon icon="upload" /> Upload Image
-                      </MDBBtn>
-                    </div>
+                    
                     {edit_button}
                   </div>
                 </div>
